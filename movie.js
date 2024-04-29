40b2986043fbfe8a1426145bfbce672b74de74f0
Elephant24456 <rlaxortn0984@naver.com>
2024-04-29 14:37:14 +0900

    >commit

--- movie.js
+++ movie.js
@@ -1,1 +1,19 @@
-alert("movie.js 연결완료");
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZTZkNjJlYmZiNDkxNmM5OThjNTg3MWYyYjM1MmI0ZiIsInN1YiI6IjY2MmE0YmZjZjcwNmRlMDExZTRmZjg3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EhGxKI5JoznlILY4DoELYXqrRv9-ZPsJ5TwhC97ehTQ'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+    alert(`영화id: $`);
+
+// 검색 구현
+// 1. 영화카드리스트 선택
+// 2. 영화카드리스트 클릭하면 영화상세페이지로 이동
+// 3. 영화상세페이지에서 영화정보 출력
